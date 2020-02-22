8fccec83ff8ba50e303f4de4859065f5726394f0
JamesOttoJ <55111157+JamesOttoJ@users.noreply.github.com>
2020-02-21 18:45:53 -0600

    Header Tabs

--- Tab_Manager.js
+++ Tab_Manager.js
@@ -17,5 +17,6 @@ function openTab(evt, tabName) {
 
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
+  document.getElementById(tabName + " Head").style.display = "block";
   evt.currentTarget.className += " active";
 }
